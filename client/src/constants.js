d2a69eb5130fde921b0710e0a11d6d1e11c619ec
Arrizalv <arrizalalvin27@gmail.com>
2026-01-31 12:35:38 +0700

    1.1.2

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,25 +1,11 @@
 // src/constants.js
 export const CONTRACT_ADDRESS = "0xBF403Fa70B6DBDEFA55907a1A7B867b9beCF9861"; // GANTI DENGAN ADDRESS KONTRAK LU DARI REMIX
-export const ETHERSCAN_API_KEY = "BF6R34IPYSV7S2Z86UNU1CQCIVXHHQG1AD"; // GANTI DENGAN API KEY DARI ETHERSCAN
-
-export const CONTRACT_ABI = [[
+export const ETHERSCAN_API_KEY = import.meta.env.VITE_ETHERSCAN_API_KEY;
+export const CONTRACT_ABI = [
 	{
 		"inputs": [],
 		"stateMutability": "nonpayable",
 		"type": "constructor"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_candidateId",
-				"type": "uint256"
-			}
-		],
-		"name": "vote",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
 	},
 	{
 		"anonymous": false,
@@ -33,48 +19,6 @@ export const CONTRACT_ABI = [[
 		],
 		"name": "Voted",
 		"type": "event"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "candidates",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "id",
-				"type": "uint256"
-			},
-			{
-				"internalType": "string",
-				"name": "name",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "voteCount",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "candidatesCount",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
 	},
 	{
 		"inputs": [],
@@ -109,20 +53,14 @@ export const CONTRACT_ABI = [[
 	{
 		"inputs": [
 			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
+				"internalType": "uint256",
+				"name": "_candidateId",
+				"type": "uint256"
 			}
 		],
-		"name": "voters",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "view",
+		"name": "vote",
+		"outputs": [],
+		"stateMutability": "nonpayable",
 		"type": "function"
 	}
-]];
+];
